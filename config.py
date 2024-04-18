023028120aa1d553e7eef15d3f33c1fd87253580
AAEmara <abdelrahmanemara18@gmail.com>
2024-04-18 23:40:23 +0200

    Microblog: Updated urls for endpoints.
    Used the url_for method from flask so it
    would be more managable to control the changing
    urls than the templates HTML code.

--- config.py
+++ config.py
@@ -5,6 +5,9 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Defines the configuration of a flask application."""
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "sqlite:///" + os.path.join(basedir, "app.db")
